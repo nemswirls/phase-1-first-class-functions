5df7943fa3ebd82f4a018c1fb911f4537efffb0e
Nemanja Stankovic <nemanjas0823@hotmail.com>
2024-10-30 02:53:33 -0400

    complete lab

---
+++ index.js
@@ -0,0 +1,15 @@
+function receivesAFunction(spy){
+spy()
+}
+function returnsANamedFunction(){
+return returnsANamedFunction
+
+}
+function returnsAnAnonymousFunction(){
+    return function(){
+        
+    }
+
+    
+}
+
